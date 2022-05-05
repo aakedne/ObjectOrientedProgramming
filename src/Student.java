74b607f2db1e1f4df531223bce7a655b9af876c2
Abdikadir <54958020+aakedne@users.noreply.github.com>
2022-05-05 01:12:02 -0500

    Initial commit

---
+++ src/Student.java
@@ -0,0 +1,20 @@
+public class Student {
+    // defining field
+    int id;
+    String name;
+    float salary;
+
+    void insert(int i, String n,float s ){
+        id = i;
+        name = n;
+        salary = s;
+    }
+    void display(){
+        System.out.println(id + " " + name + " " +salary);
+    }
+
+
+
+
+    }
+
